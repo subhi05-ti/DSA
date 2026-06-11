ef366995a6a1f36b162a3fd5f386c2f9698eb688
subhi <subhi.tiwaricse2023@indoreinstitute.com>
2026-06-11 14:05:40 +0530

    Add files via upload

---
+++ negative_num_binary.cpp
@@ -0,0 +1,28 @@
+// #include<iostream>
+// #include<math.h>
+// using namespace std;
+// int main()
+// {
+//     int n,ans=0,i=0;
+//     cout<<"enter a  number";
+//     cin>>n;
+//     if(n < 0)
+//     {
+//         n = -n;
+//     }
+//     int m = ~n;
+//      cout<<m;
+//     while(m!=0)
+//     {
+//         int bit = m & 1;
+//         ans = bit*(pow(10,i)) + ans;
+//         m = m>>1;
+//         i++;
+//     }
+//     //-7
+//     ans = ans+1;
+//     cout<<ans;
+// }
+
+
+
