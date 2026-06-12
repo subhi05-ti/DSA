41835dbd1229df34f0e543c5c53e2991626cd8b3
subhi <subhi.tiwaricse2023@indoreinstitute.com>
2026-06-12 08:45:02 +0530

    Add files via upload

---
+++ reverse_array.cpp
@@ -0,0 +1,31 @@
+// //  REVERSING THE ARRAY
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int a[9],i;
+//     cout<<"enter element in array"<<endl;
+//     for(i=0 ; i<9 ; i++)
+//     {
+//         cin>>a[i];
+//     }
+//     int low = 0,high =8,n;
+//     while(low<=high)
+//     {
+//         // n =a[low];
+//         // a[low] = a[high];
+//         // a[high] = n;
+
+//         swap(a[low],a[high]);     // USING SWAP FUNCTION 
+
+//         low++;
+//         high--;
+//     }
+//     for(i=0 ; i<9 ; i++)
+//     {
+//         cout<<a[i]<<endl;
+//     }
+   
+// }
+
