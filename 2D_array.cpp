025eff96c40f21f650529c6f04331395c6dd9d1c
subhi <subhi.tiwaricse2023@indoreinstitute.com>
2026-07-16 13:20:36 +0530

    Add files via upload

---
+++ 2D_array.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int row,col;
+    cout<<"entee no of rows and columns "<<endl;
+    cin>>row>>col;
+    
+    // creating dynamic 2D array
+    int** arr = new int *[row];
+    for(int i=0 ; i<row ; i++)
+    {
+        arr[i] = new int[col];
+    }
+
+    // Taking input
+    for(int i=0 ; i<row ; i++)
+    {
+        for(int j =0 ; j<col ; j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+
+    // printing 2D array
+    for(int i=0 ; i<row ; i++)
+    {
+        for(int j =0 ; j<col ; j++)
+        {
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    // releasing he memory
+     for(int i=0 ; i<row ; i++)
+    {
+        delete [] arr[i];
+    }
+    delete [] arr;
+    
+}
