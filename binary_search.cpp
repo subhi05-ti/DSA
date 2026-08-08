561eb7c40b172f7ab9cf61b85223a3b554ba6318
subhi <subhi.tiwaricse2023@indoreinstitute.com>
2026-08-08 21:56:08 +0530

    Add files via upload

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,36 +1,68 @@
 #include<iostream>
 using namespace std;
+
+void print(int arr[],int s,int e)
+{
+    for(int i=s ; i<=e ; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+bool binary_search(int arr[],int n,int key,int s,int e)
+{
+
+// printing
+print(arr,s,e);
+
+    if(s>e)
+    {
+        return 0;
+    }
+    int mid = (s+e)/2;
+
+    cout<<"value of mid "<<mid<<endl;
+
+    if(key == arr[mid])
+    {
+        return 1;
+    }
+    if(key>arr[mid])
+    {
+        s = mid+1;
+        return binary_search(arr,n,key,s,e);
+    }
+    else
+    {
+      e = mid -1;
+      return binary_search(arr,n,key,s,e); 
+    }
+}
+
 int main()
 {
-    int a[10],mid,s=0,e=9;
-    cout<<"enter element in array"<<endl;
-    for(int i=0; i<10 ; i++)
-    {
-        cin>>a[i];
-    }
     int n;
-    cout<<"enter number for searching"<<endl;
+    cout<<"enter the size of array"<<endl;
     cin>>n;
-    while(s<=e)
+    int *arr = new int[n];
+    // int arr[5] = { 3,8,19,55,90};
+    for(int i=0 ; i<5 ; i++)
     {
-        // IN SOME CASES THE VALUE OF MID EXCEED THE INT LIMIT SO 
-       // mid = (s+e)/2;
-
-       // INSTEAD OF THIS WE GONNA DO
-
-       mid = s +(e-s)/2;
-        if(n == a[mid])
-        {
-            cout<<"element found at index = "<<mid;
-            break;
-        }
-        else if(n>a[mid])
-        {
-            s = mid+1;
-        }
-        else
-        {
-            e = mid-1;
-        }
+        cin>>arr[i];
     }
+    int s =0, e=n-1;
+    int key;
+    cout<<"enter element to be searched"<<endl;
+    cin>>key;;
+    int result = binary_search(arr,n,key,0,n-1);
+    if(result)
+    {
+        cout<<"found";
+    }
+    else
+    {
+        cout<<"not found";
+    }
+
 }
